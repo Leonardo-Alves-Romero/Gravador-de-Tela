7eb941ba272980f5c36819efbf340bff59cb178c
Leonardo Alves De Oliveira Romero <leonardo_romero_ti@outlook.com>
2023-03-24 20:30:57 -0300

    Create GravadorTela.py

---
+++ GravadorTela.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+import pyautogui
+
+SCREEN_SIZE = (1980, 1080)
+fourcc = cv2.VideoWriter_fourcc(*"XVID")
+out = cv2.VideoWriter("gravacao.avi", fourcc, 20.0, (SCREEN_SIZE))
+
+while True:
+    img = pyautogui.screenshot()
+    frame = np.array(img)
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    out.write(frame)
+
+    if cv2.waitkey(1) == ord("q"):
+        break
+
+cv2.destroyallwindows()
+out.release()
